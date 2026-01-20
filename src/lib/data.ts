3ebb9008817c83b8720cf48e20626f05f738f2de
RongKai <zjlxxrk@gmail.com>
2026-01-20 20:02:06 +1300

    update questions

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -5,32 +5,144 @@ export const surveyData: SurveyGroup[] = [
     id: 'group-1',
     name: '场景1',
     images: [
-      { id: 'p1', url: 'alma.jpg' },
-      { id: 'p2', url: 'https://raw.githubusercontent.com/mpetroff/pannellum/refs/heads/master/examples/examplepanocube0.jpg' }
+      { id: 'p1', url: 'v7_00029_03005_221847.jpg' },
+      { id: 'p2', url: 'dreamcube_00029_03005_221847.jpg' },
+      { id: 'p3', url: 'panodiff_00029_03005_221847.jpg' },
+      { id: 'p4', url: 'panodiffusion_00029_03005_221847.jpg' },
+      { id: 'p5', url: 'par_00029_03005_221847.jpg' },
+      { id: 'p6', url: 'flux_00029_03005_221847.jpg' },
     ],
     questions: [
-      { id: 'q1', text: '哪个场景的内容' },
-      { id: 'q2', text: '请描述你在场景中观察到的主要色调：' },
-      { id: 'q3', text: '请描述你在场景中观察到的主要色调：' },
-      { id: 'q4', text: '请描述你在场景中观察到的主要色调：' }
+      { id: 'q1', text: '请选择色彩过渡和光影最自然的场景' },
+      { id: 'q2', text: '请选择纹理细节最清晰丰富的场景' },
+      { id: 'q3', text: '请选择拉伸畸变（扭曲）感最轻微的场景' },
+      { id: 'q4', text: '综合来看，请选择整体视觉质量最高的场景' }
     ]
   },
   {
     id: 'group-2',
     name: '场景2',
     images: [
-      { id: 'p1', url: 'https://raw.githubusercontent.com/mpetroff/pannellum/refs/heads/master/examples/examplepanocube1.jpg' },
-      { id: 'p2', url: 'https://raw.githubusercontent.com/mpetroff/pannellum/refs/heads/master/examples/examplepanocube2.jpg' },
-      { id: 'p3', url: 'https://raw.githubusercontent.com/mpetroff/pannellum/refs/heads/master/examples/examplepanocube3.jpg' },
-      { id: 'p4', url: 'https://raw.githubusercontent.com/mpetroff/pannellum/refs/heads/master/examples/examplepanocube3.jpg' },
-      { id: 'p5', url: 'https://raw.githubusercontent.com/mpetroff/pannellum/refs/heads/master/examples/examplepanocube3.jpg' },
-      { id: 'p6', url: 'https://raw.githubusercontent.com/mpetroff/pannellum/refs/heads/master/examples/examplepanocube3.jpg' },
+      { id: 'p1', url: 'v7_00205_03029_1001143.jpg' },
+      { id: 'p2', url: 'dreamcube_00205_03029_1001143.jpg' },
+      { id: 'p3', url: 'panodiff_00205_03029_1001143.jpg' },
+      { id: 'p4', url: 'panodiffusion_00205_03029_1001143.jpg' },
+      { id: 'p5', url: 'par_00205_03029_1001143.jpg' },
+      { id: 'p6', url: 'flux_00205_03029_1001143.jpg' },
     ],
     questions: [
-      { id: 'q1', text: '哪个场景让你感到更舒适？' },
-      { id: 'q2', text: '请描述你在场景中观察到的主要色调：' },
-      { id: 'q3', text: '请描述你在场景中观察到的主要色调：' },
-      { id: 'q4', text: '请描述你在场景中观察到的主要色调：' }
+      { id: 'q1', text: '请选择色彩过渡和光影最自然的场景' },
+      { id: 'q2', text: '请选择纹理细节最清晰丰富的场景' },
+      { id: 'q3', text: '请选择拉伸畸变（扭曲）感最轻微的场景' },
+      { id: 'q4', text: '综合来看，请选择整体视觉质量最高的场景' }
     ]
-  }
+  },
+  {
+    id: 'group-3',
+    name: '场景3',
+    images: [
+      { id: 'p1', url: 'v7_00653_03089_682.jpg' },
+      { id: 'p2', url: 'dreamcube_00653_03089_682.jpg' },
+      { id: 'p3', url: 'panodiff_00653_03089_682.jpg' },
+      { id: 'p4', url: 'panodiffusion_00653_03089_682.jpg' },
+      { id: 'p5', url: 'par_00653_03089_682.jpg' },
+      { id: 'p6', url: 'flux_00653_03089_682.jpg' },
+    ],
+    questions: [
+      { id: 'q1', text: '请选择色彩过渡和光影最自然的场景' },
+      { id: 'q2', text: '请选择纹理细节最清晰丰富的场景' },
+      { id: 'q3', text: '请选择拉伸畸变（扭曲）感最轻微的场景' },
+      { id: 'q4', text: '综合来看，请选择整体视觉质量最高的场景' }
+    ]
+  },
+  {
+    id: 'group-4',
+    name: '场景4',
+    images: [
+      { id: 'p1', url: 'v7_02315_030541.jpg' },
+      { id: 'p2', url: 'dreamcube_02315_030541.jpg' },
+      { id: 'p3', url: 'panodiff_02315_030541.jpg' },
+      { id: 'p4', url: 'panodiffusion_02315_030541.jpg' },
+      { id: 'p5', url: 'par_02315_030541.jpg' },
+      { id: 'p6', url: 'flux_02315_030541.jpg' },
+    ],
+    questions: [
+      { id: 'q1', text: '请选择色彩过渡和光影最自然的场景' },
+      { id: 'q2', text: '请选择纹理细节最清晰丰富的场景' },
+      { id: 'q3', text: '请选择拉伸畸变（扭曲）感最轻微的场景' },
+      { id: 'q4', text: '综合来看，请选择整体视觉质量最高的场景' }
+    ]
+  },
+  {
+    id: 'group-5',
+    name: '场景5',
+    images: [
+      { id: 'p1', url: 'v7_03075_031301.jpg' },
+      { id: 'p2', url: 'dreamcube_03075_031301.jpg' },
+      { id: 'p3', url: 'panodiff_03075_031301.jpg' },
+      { id: 'p4', url: 'panodiffusion_03075_031301.jpg' },
+      { id: 'p5', url: 'par_03075_031301.jpg' },
+      { id: 'p6', url: 'flux_03075_031301.jpg' },
+    ],
+    questions: [
+      { id: 'q1', text: '请选择色彩过渡和光影最自然的场景' },
+      { id: 'q2', text: '请选择纹理细节最清晰丰富的场景' },
+      { id: 'q3', text: '请选择拉伸畸变（扭曲）感最轻微的场景' },
+      { id: 'q4', text: '综合来看，请选择整体视觉质量最高的场景' }
+    ]
+  },
+  {
+    id: 'group-6',
+    name: '场景6',
+    images: [
+      { id: 'p1', url: 'v7_02442_030668.jpg' },
+      { id: 'p2', url: 'dreamcube_02442_030668.jpg' },
+      { id: 'p3', url: 'panodiff_02442_030668.jpg' },
+      { id: 'p4', url: 'panodiffusion_02442_030668.jpg' },
+      { id: 'p5', url: 'par_02442_030668.jpg' },
+      { id: 'p6', url: 'flux_02442_030668.jpg' },
+    ],
+    questions: [
+      { id: 'q1', text: '请选择色彩过渡和光影最自然的场景' },
+      { id: 'q2', text: '请选择纹理细节最清晰丰富的场景' },
+      { id: 'q3', text: '请选择拉伸畸变（扭曲）感最轻微的场景' },
+      { id: 'q4', text: '综合来看，请选择整体视觉质量最高的场景' }
+    ]
+  },
+  {
+    id: 'group-7',
+    name: '场景7',
+    images: [
+      { id: 'p1', url: 'v7_2.jpg' },
+      { id: 'p2', url: 'dreamcube_2.jpg' },
+      { id: 'p3', url: 'panodiff_2.jpg' },
+      { id: 'p4', url: 'panodiffusion_2.jpg' },
+      { id: 'p5', url: 'par_2.jpg' },
+      { id: 'p6', url: 'flux_2.jpg' },
+    ],
+    questions: [
+      { id: 'q1', text: '请选择色彩过渡和光影最自然的场景' },
+      { id: 'q2', text: '请选择纹理细节最清晰丰富的场景' },
+      { id: 'q3', text: '请选择拉伸畸变（扭曲）感最轻微的场景' },
+      { id: 'q4', text: '综合来看，请选择整体视觉质量最高的场景' }
+    ]
+  },
+  {
+    id: 'group-8',
+    name: '场景8',
+    images: [
+      { id: 'p1', url: 'v7_52939.jpg' },
+      { id: 'p2', url: 'dreamcube_52939.jpg' },
+      { id: 'p3', url: 'panodiff_52939.jpg' },
+      { id: 'p4', url: 'panodiffusion_52939.jpg' },
+      { id: 'p5', url: 'par_52939.jpg' },
+      { id: 'p6', url: 'flux_52939.jpg' },
+    ],
+    questions: [
+      { id: 'q1', text: '请选择色彩过渡和光影最自然的场景' },
+      { id: 'q2', text: '请选择纹理细节最清晰丰富的场景' },
+      { id: 'q3', text: '请选择拉伸畸变（扭曲）感最轻微的场景' },
+      { id: 'q4', text: '综合来看，请选择整体视觉质量最高的场景' }
+    ]
+  },
 ];
